78eca6e335043d8185a4c01548bd31910818c38d
Fitzgerald Afful <fitzafful@gmail.com>
2022-03-11 21:40:56 +0000

    Initial Commit

---
+++ SubMan/SubManApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubManApp.swift
+//  SubMan
+//
+//  Created by Fitzgerald Afful on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SubManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
